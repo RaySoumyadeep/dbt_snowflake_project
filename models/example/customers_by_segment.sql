203d8074407378bbab991947b3bb9364db28b63f
Soumyadeep Ray <89061659+RaySoumyadeep@users.noreply.github.com>
2023-05-09 00:38:28 -0500

    Delete models/example directory

--- models/example/customers_by_segment.sql
+++
@@ -1,6 +0,0 @@
-{{ config(materialized='table') }}
-select
-    c_mktsegment as market_segment
-    , count(c_custkey) as number_of_customers
-from {{ source('TPCH_SF1', 'CUSTOMER')}}
-group by c_mktsegment
