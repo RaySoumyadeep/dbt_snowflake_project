843253585a27c3bb5b1a3e0a8ce0ac249731ef79
Soumyadeep Ray <89061659+RaySoumyadeep@users.noreply.github.com>
2023-05-09 00:36:32 -0500

    Delete target directory

--- target/run/demo_dbt/models/example/my_second_dbt_model.sql
+++
@@ -1,8 +0,0 @@
-
-  create or replace  view ANALYTICS.PUBLIC.my_second_dbt_model  as (
-    -- Use the `ref` function to select from other models
-
-select *
-from ANALYTICS.PUBLIC.my_first_dbt_model
-where id = 1
-  );
